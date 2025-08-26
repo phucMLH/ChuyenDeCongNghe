b133714fb98ee33d403a04a99ab3de814b18b19b
cestunchat <mailehongphuc52003@gmail.com>
2025-08-26 19:21:10 +0700

    View Funtions

--- views/polls/urls.py
+++ views/polls/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path("articles/<int:year>/", views.year_archive),
     path("articles/<int:year>/<int:month>/", views.month_archive),
     path("articles/<int:year>/<int:month>/<slug:slug>/", views.article_detail),
+    path("now/", views.current_datetime, name="current_datetime"),
+    path("notfound/", views.not_found_view, name="not_found"),
+    path("forbidden/", views.permission_denied_view, name="forbidden"),
+    path("created/", views.created_view, name="created"),
+    path("asyncnow/", views.async_current_datetime, name="async_current_datetime"),
 ]
--- views/polls/views.py
+++ views/polls/views.py
@@ -11,3 +11,26 @@ def month_archive(request, year, month):
 
 def article_detail(request, year, month, slug):
     return HttpResponse(f"Article detail: {year}/{month}/{slug}")
+
+import datetime
+from django.http import Http404
+from django.core.exceptions import PermissionDenied
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = f'<html lang="en"><body>It is now {now}.</body></html>'
+    return HttpResponse(html)
+
+def not_found_view(request):
+    raise Http404("This page does not exist")
+
+def permission_denied_view(request):
+    raise PermissionDenied
+
+def created_view(request):
+    return HttpResponse("Created!", status=201)
+
+async def async_current_datetime(request):
+    now = datetime.datetime.now()
+    html = f'<html lang="en"><body>It is now {now}.</body></html>'
+    return HttpResponse(html)

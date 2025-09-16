908a1b91dc46a43060946b65681ad5f4a22949a9
cestunchat <mailehongphuc52003@gmail.com>
2025-09-16 15:02:11 +0700

    Tutorial 1: App Snippets

--- drftutorial/tutorial/settings.py
+++ drftutorial/tutorial/settings.py
@@ -38,11 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'snippets',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
 }
 
 MIDDLEWARE = [
--- drftutorial/tutorial/urls.py
+++ drftutorial/tutorial/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
-
+from django.contrib import admin
+from django.urls import path, include
 from tutorial.quickstart import views
 
 router = routers.DefaultRouter()
@@ -10,6 +11,8 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('', include('snippets.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]

aece99ceca8f03ae4fbae58766eb8711cdf85378
cestunchat <mailehongphuc52003@gmail.com>
2025-10-07 13:23:23 +0700

    Edit view for caching

--- rest_server/apiapp/views.py
+++ rest_server/apiapp/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
+from django.utils import timezone
 
 from .models import Item
 from .serializers import ItemSerializer
@@ -18,4 +19,10 @@ class ItemViewSet(viewsets.ModelViewSet):
     # Cache the list view for 60 seconds as an example
     @method_decorator(cache_page(60))
     def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+        # Call the parent to get the normal response
+        response = super().list(request, *args, **kwargs)
+        # Attach a debug header indicating when this response was generated.
+        # This header will be cached together with the response, so it helps
+        # determine whether subsequent requests returned a cached response.
+        response['X-Cached-At'] = timezone.now().isoformat()
+        return response
